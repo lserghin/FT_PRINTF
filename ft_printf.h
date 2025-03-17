42879484172734c54475a9c3afdc65912bef9df3
Loukmane Serghini <lserghin@c2r1p1.1337.ma>
2025-03-17 21:25:21 +0000

    ✅

--- ft_printf.h
+++ ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lserghin <lserghin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:47:15 by lserghin          #+#    #+#             */
-/*   Updated: 2024/11/28 00:30:29 by lserghin         ###   ########.fr       */
+/*   Updated: 2025/03/17 21:23:26 by lserghin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <stdlib.h>
-# include <stdio.h>
 # include <unistd.h>
 
 int	ft_printf(const char *Format, ...);
 int	ft_putunbr(unsigned int unbr);
-int	ft_putptr(void *ptr);
 int	ft_putuhex(unsigned int nb);
 int	ft_putlhex(unsigned int nb);
+int	ft_putptr(void *ptr);
+int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_putnbr(int nb);
-int	ft_putchar(char c);
 
 #endif
